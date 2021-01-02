fc34751b035899f2de24c4c88a028c1269fb2bbd
Arjun Rao <arjun.rao@placeexchange.com>
2021-01-01 23:52:39 -0500

    refactor: remove unwanted string parsing & dom removals

--- src/js/post.js
+++ src/js/post.js
@@ -4,14 +4,12 @@ function cleanMarkdownForPost() {
         const frontmatter = md["children"][1].textContent.split("\n")
         date = frontmatter[0].split(":")[1].slice(2, -1);
         raw_title = frontmatter[2].split(":")[1].slice(2, -1);
-        title = raw_title.replace(/\s+/g, '-').toLowerCase();
-        category = frontmatter[3].split(":")[1].slice(2, -1);
-        post_url = date + "/" + title
-        // remove front matter
-        md["childNodes"][0].remove()
-        md["childNodes"][0].remove()
-        md["childNodes"][0].remove()
-        md["childNodes"][0].remove()
+        let i = 0;
+        do {
+            i = i + 1;
+            // remove front matter
+            md["childNodes"][0].remove()
+        } while (i < 3);
         const titleNode = document.createElement("h2")
         titleNode.textContent = raw_title
         md.replaceChild(titleNode, md.children[0]);
